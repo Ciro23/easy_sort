96d07fc1e45984c4e0f2ce9b69cd308c26452f97
Ciro23 <ciro.sorre23@gmail.com>
2022-12-03 01:09:16 +0100

    better variable name

--- src/main/java/it/tino/easysort/algorithms/BubbleSort.java
+++ src/main/java/it/tino/easysort/algorithms/BubbleSort.java
@@ -9,15 +9,15 @@ public class BubbleSort<T extends Comparable<T>> implements Sort<T> {
 
     @Override
     public void sort(List<T> list) {
-        boolean bool = true;
-        while (bool) {
-            bool = false;
+        boolean shouldSort = true;
+        while (shouldSort) {
+            shouldSort = false;
             for (int i = 0; i < list.size() - 1; i++) {
                 T current = list.get(i);
                 T successor = list.get(i + 1);
 
                 if (current.compareTo(successor) > 0) {
-                    bool = true;
+                    shouldSort = true;
                     Collections.swap(list, i, i + 1);
                 }
             }
