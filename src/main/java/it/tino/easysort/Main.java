489b3e6535c09a7adbe07c6a69479045c4ac1b18
Ciro23 <ciro.sorre23@gmail.com>
2022-12-05 22:32:56 +0100

    created corrected implementation

--- src/main/java/it/tino/easysort/Main.java
+++ src/main/java/it/tino/easysort/Main.java
@@ -1,6 +1,7 @@
 package it.tino.easysort;
 
 import it.tino.easysort.algorithms.BubbleSort;
+import it.tino.easysort.algorithms.InsertionSort;
 import it.tino.easysort.interfaces.ListReader;
 import it.tino.easysort.interfaces.Menu;
 import it.tino.easysort.interfaces.RealTimeListReader;
@@ -14,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         SortingAlgorithm<String> bubbleSort = new BubbleSort<>();
-        SortingAlgorithm<String> insertionSort = new BubbleSort<>();
+        SortingAlgorithm<String> insertionSort = new InsertionSort<>();
         ListReader textFileReader = new TextFileReader();
         RealTimeListReader commandLineReader = new CommandLineReader(System.out, new Scanner(System.in));
 
